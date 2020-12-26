016b72ab687ab88921b3de9769b604a62de13228
Telat Kaya <telatkaya@yandex.com>
2020-12-27 02:27:31 +0300

    add vegas.js

--- SemanticUiCoreBoilerPlate/wwwroot/js/main.js
+++ SemanticUiCoreBoilerPlate/wwwroot/js/main.js
@@ -12,3 +12,19 @@ $(".following-menu-fix").visibility({
 $(".toc.item").on("click", function () {
 	$(".ui.sidebar").sidebar("toggle");
 });
+
+$(".following-menu-fix").vegas({
+	preload: true,
+	delay:10000,
+	slides: [
+		{ src: "/assets/img/10.jpg" },
+		{ src: "/assets/img/11.jpg" },
+		{ src: "/assets/img/12.jpg" },
+		{ src: "/assets/img/13.jpg" }
+	]
+});
+
+$.vegas.isVideoCompatible = function () {
+	var devices = /(Android|webOS|Phone|iPad|iPod|BlackBerry|Windows Phone)/i;
+	return !devices.test(navigator.userAgent);
+}
